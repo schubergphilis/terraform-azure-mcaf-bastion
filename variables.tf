b36eb51d286f5d1636558b6ce90d488a72231187
atmahussien <ahussien@schubergphilis.com>
2024-11-04 11:31:29 +0100

    Added Public IP Object as an extra variable

--- variables.tf
+++ variables.tf
@@ -7,21 +7,30 @@ variable "bastion" {
     resource_group_name = string # The name of the resource group.
 
     ip_configuration = object({
-      name                 = string # The name of the IP configuration.
-      subnet_id            = string # The ID of the subnet for the Bastion Host.
-      public_ip_address_id = string # The ID of the public IP address associated with the Azure Bastion Host.
+      name      = string # The name of the IP configuration.
+      subnet_id = string # The ID of the subnet for the Bastion Host.
+
+      public_ip = object({
+        name                         = string            # The name of the Public IP.
+        allocation_method            = string            # Allocation method: 'Static' or 'Dynamic'.
+        sku                          = string            # SKU: 'Basic' or 'Standard'.
+        idle_timeout_in_minutes      = optional(number)  # Idle timeout in minutes.
+        tags                         = optional(map(string)) # Tags for the Public IP.
+        zones                        = optional(list(string)) # Availability zones.
+      })
     })
 
-    copy_paste_enabled     = bool        # Enable or disable copy-paste functionality.
-    file_copy_enabled      = bool        # Enable or disable file copy functionality.
-    ip_connect_enabled     = bool        # Enable or disable IP connect.
-    kerberos_enabled       = bool        # Enable or disable Kerberos authentication.
-    scale_units            = number      # The number of scale units.
-    shareable_link_enabled = bool        # Enable or disable shareable links.
-    sku                    = string      # The SKU of the Bastion Host.
-    tunneling_enabled      = bool        # Enable or disable tunneling.
-    virtual_network_id     = string      # The ID of the virtual network.
-    tags                   = map(string) # A mapping of tags to assign to the resource.
+    # Optional attributes for the Bastion Host (defaults will be used if omitted)
+    copy_paste_enabled     = optional(bool, false)   # Enable or disable copy-paste functionality.
+    file_copy_enabled      = optional(bool, false)   # Enable or disable file copy functionality.
+    ip_connect_enabled     = optional(bool, false)   # Enable or disable IP connect.
+    kerberos_enabled       = optional(bool, false)   # Enable or disable Kerberos authentication.
+    scale_units            = optional(number, 2)     # The number of scale units.
+    shareable_link_enabled = optional(bool, false)   # Enable or disable shareable links.
+    sku                    = optional(string, "Standard") # The SKU of the Bastion Host.
+    tunneling_enabled      = optional(bool, false)   # Enable or disable tunneling.
+    virtual_network_id     = optional(string)        # The ID of the virtual network.
+    tags                   = optional(map(string))   # Tags to assign to the Bastion Host.
   }))
 
   default = {}
