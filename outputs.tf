1a7a0351500644ca2a6512fced1e569bf52560c7
atmahussien <ahussien@schubergphilis.com>
2024-11-04 11:32:08 +0100

    updated outputs with new logic post public ip integration

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
-output "ids" {
+output "bastion_host_ids" {
   description = "A map of Azure Bastion Host IDs."
   value       = { for k, bastion in azurerm_bastion_host.this : k => bastion.id }
 }
 
-output "names" {
+output "bastion_host_names" {
   description = "A map of Azure Bastion Host names."
   value       = { for k, bastion in azurerm_bastion_host.this : k => bastion.name }
+}
+
+output "public_ip_ids" {
+  description = "A map of Public IP IDs."
+  value       = { for k, pip in azurerm_public_ip.public_ip : k => pip.id }
+}
+
+output "public_ip_addresses" {
+  description = "A map of Public IP addresses."
+  value       = { for k, pip in azurerm_public_ip.public_ip : k => pip.ip_address }
 }
