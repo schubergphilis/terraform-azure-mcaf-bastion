2d83047e639f2805518e94cf916e748c944c79d9
atmahussien <ahussien@schubergphilis.com>
2024-11-05 10:22:38 +0100

    debugged main.tf

--- main.tf
+++ main.tf
@@ -10,42 +10,44 @@ terraform {
 }
 
 provider "azurerm" {
-  features = {}
+  features {}
 }
 
 resource "azurerm_resource_group" "this" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = var.bastion.resource_group_name
+  location = var.bastion.location
 }
 
 resource "azurerm_public_ip" "this" {
-  name                = var.public_ip_name
+  name                = lookup(var.bastion, "public_ip_name", "bastion-pip")
   resource_group_name = azurerm_resource_group.this.name
-  location            = var.location
+  location            = var.bastion.location
   allocation_method   = "Static"
   sku                 = "Standard"
 
-  idle_timeout_in_minutes = var.idle_timeout_in_minutes
+  idle_timeout_in_minutes = lookup(var.bastion, "idle_timeout_in_minutes", 4)
 
-  tags = var.tags
+  tags = lookup(var.bastion, "tags", {})
+
+  # If you need to handle optional zones or other attributes, you can add them here using lookup()
 }
 
 resource "azurerm_bastion_host" "this" {
-  name                = var.bastion_name
-  location            = var.location
+  name                = lookup(var.bastion, "bastion_name", "bastion-host")
+  location            = var.bastion.location
   resource_group_name = azurerm_resource_group.this.name
 
-  copy_paste_enabled = var.copy_paste_enabled
-  file_copy_enabled  = var.file_copy_enabled
-  scale_units        = var.scale_units
-  sku                = "Standard"
-  virtual_network_id = var.virtual_network_id
+  copy_paste_enabled     = lookup(var.bastion, "copy_paste_enabled", true)
+  file_copy_enabled      = lookup(var.bastion, "file_copy_enabled", true)
+  scale_units            = lookup(var.bastion, "scale_units", 2)
+  sku                    = "Standard"
+  virtual_network_id     = lookup(var.bastion, "virtual_network_id", null)
 
-  tags = var.tags
+  tags = lookup(var.bastion, "tags", {})
 
   ip_configuration {
     name                 = "ipconfig1"
-    subnet_id            = var.subnet_id
+    subnet_id            = var.bastion.subnet_id
     public_ip_address_id = azurerm_public_ip.this.id
   }
 }
